ff68cb5de0f7808e9fdffeaf018776c752020816
Mykola KOBAK <mykola.kobak@smile-ukraine.com>
2021-08-15 16:59:47 +0300

    Initialized project, put categories and special field type

---
+++ web/modules/custom/nyan_site/src/Plugin/Field/FieldType/EntityReferenceWithQuantity.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Drupal\nyan_site\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Provides list of entity reference items with quantity.
+ *
+ * @FieldType(
+ *   id = "entity_reference_with_quantity",
+ *   label = @Translation("Entity reference with quantity"),
+ *   category = @Translation("Custom"),
+ *   default_widget = "entity_reference_with_quantity",
+ *   default_formatter = "entity_reference_label",
+ *   list_class = "\Drupal\Core\Field\EntityReferenceFieldItemList",
+ * )
+ */
+class EntityReferenceWithQuantity extends EntityReferenceItem {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(
+    FieldStorageDefinitionInterface $field_definition
+  ) {
+    $properties = parent::propertyDefinitions($field_definition);
+
+    $properties['quantity'] = DataDefinition::create('integer')
+      ->setLabel(t('Quantity'))
+      ->setRequired(TRUE);
+    $properties['price'] = DataDefinition::create('string')
+      ->setLabel(t('Price'));
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(
+    FieldStorageDefinitionInterface $field_definition
+  ) {
+    $schema = parent::schema($field_definition);
+    $schema['columns'] += [
+      'quantity' => [
+        'type' => 'int',
+        'size' => 'normal',
+      ],
+      'price' => [
+        'type' => 'numeric',
+        'not_null' => TRUE,
+        'default' => 0,
+        'unsigned' => TRUE,
+      ],
+    ];
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultFieldSettings() {
+    return [
+      'quantity_settings' => [],
+    ] + parent::defaultFieldSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function mainPropertyName() {
+    return NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fieldSettingsForm(
+    array $form,
+    FormStateInterface $form_state
+  ) {
+    $form = parent::fieldSettingsForm($form, $form_state);
+    $form['quantity_settings'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Quantity settings'),
+      '#open' => TRUE,
+      '#tree' => TRUE,
+    ];
+    $quantity_settings = $this->getSetting('quantity_settings');
+    $form['quantity_settings']['title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Quantity title'),
+      '#default_value' => $quantity_settings['title'] ?? '',
+    ];
+    $form['quantity_settings']['description'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Quantity description'),
+      '#default_value' => $quantity_settings['description'] ?? '',
+    ];
+    $form['quantity_settings']['default_value'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Quantity default value'),
+      '#default_value' => $quantity_settings['default_value'] ?? '',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConstraints() {
+    $constraint_manager = \Drupal::typedDataManager()->getValidationConstraintManager();
+    $constraints = parent::getConstraints();
+
+    $constraints[] = $constraint_manager->create('ComplexData', [
+      'price' => [
+        'Regex' => [
+          'pattern' => '/^[+-]?((\d+(\.\d*)?)|(\.\d+))$/i',
+        ],
+      ],
+    ]);
+
+    return $constraints;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getPreconfiguredOptions() {
+    // Provide no preconfigured options to avoid flooding the list of field
+    // types.
+    return [];
+  }
+
+}
---
+++ web/modules/custom/nyan_site/src/Plugin/Field/FieldWidget/EntityReferenceWithQuantityWidget.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\nyan_site\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\Plugin\Field\FieldWidget\EntityReferenceAutocompleteWidget;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Provides widget for entity reference with quantity field type.
+ *
+ * @FieldWidget(
+ *   id = "entity_reference_with_quantity",
+ *   label = @Translation("Entity reference with quantity"),
+ *   field_types = {
+ *     "entity_reference_with_quantity"
+ *   }
+ * )
+ */
+class EntityReferenceWithQuantityWidget extends EntityReferenceAutocompleteWidget {
+
+  use StringTranslationTrait;
+
+  /**
+   * Quantity minimal value.
+   */
+  const QUANTITY_MIN_VALUE = 0;
+
+  /**
+   * Field quantity weight.
+   */
+  const FIELD_QUANTITY_WEIGHT = 10;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(
+    FieldItemListInterface $items,
+    $delta,
+    array $element,
+    array &$form,
+    FormStateInterface $form_state
+  ) {
+    $widget = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Supplies info'),
+    ];
+    $fields = parent::formElement(
+      $items,
+      $delta,
+      $element,
+      $form,
+      $form_state
+    );
+
+    $quantity_settings = $this->fieldDefinition->getSetting('quantity_settings');
+    $default_value = isset($items[$delta]) ?
+      $items[$delta]->quantity : $quantity_settings['default_value'];
+
+    $title = $quantity_settings['title'] ?? '';
+    $fields['quantity'] = [
+      '#title' => $title,
+      '#type' => 'number',
+      '#default_value' => $default_value,
+      '#min' => static::QUANTITY_MIN_VALUE,
+      '#weight' => static::FIELD_QUANTITY_WEIGHT,
+    ];
+    $fields['price'] = [
+      '#title' => $this->t('Price'),
+      '#type' => 'number',
+      '#step' => 'any',
+      '#default_value' => $items[$delta]->price ?? 0,
+    ];
+    $widget += $fields;
+
+    return $widget;
+  }
+
+}
